28f1cd70f9706cd27d072eb52f7a901f40c208f5
tmdgus1715 <tmdgus1715@gmail.com>
2023-12-16 16:27:15 +0900

    test

---
+++ src/main/java/org/example/bcsdaoptest/BcsdaoptestApplication.java
@@ -0,0 +1,13 @@
+package org.example.bcsdaoptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcsdaoptestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BcsdaoptestApplication.class, args);
+    }
+
+}
